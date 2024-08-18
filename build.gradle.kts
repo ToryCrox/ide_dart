f93680c14820898868635b228ffd978e30733675
ToryCrox <exceedutow@gmail.com>
2024-08-18 18:40:33 +0800

    更新工具

--- build.gradle.kts
+++ build.gradle.kts
@@ -40,6 +40,26 @@ dependencies {
         instrumentationTools()
     }
 }
+// Configure IntelliJ Platform Gradle Plugin - read more: https://plugins.jetbrains.com/docs/intellij/tools-intellij-platform-gradle-plugin-extension.html
+intellijPlatform {
+
+    publishing {
+        token = System.getenv("ORG_GRADLE_PROJECT_intellijPublishToken")
+
+        // The pluginVersion is based on the SemVer (https://semver.org) and supports pre-release labels, like 2.1.7-alpha.3
+        // Specify pre-release label to publish the plugin in a custom Release Channel automatically. Read more:
+        // https://plugins.jetbrains.com/docs/intellij/deployment.html#specifying-a-release-channel
+        channels = providers.gradleProperty("pluginVersion").map { listOf(it.substringAfter('-', "").substringBefore('.').ifEmpty { "default" }) }
+    }
+
+    pluginVerification {
+        ides {
+            recommended()
+        }
+    }
+}
+
+
 
 tasks.test {
     useJUnitPlatform()
